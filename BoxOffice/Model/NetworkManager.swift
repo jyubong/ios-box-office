db31286921fd791edbeb4bcc90d66482e84dd9d9
jyubong <nane030@naver.com>
2023-11-30 17:02:28 +0900

    refactor: NetworkManager concurrency로 수정

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -14,61 +14,41 @@ final class NetworkManager {
     
     private init() { }
     
-    func fetchDailyBoxOffice(at date: String, completion: @escaping (Result<[DailyBoxOfficeList], Error>) -> Void) {
-        guard let url = URL(string: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)") else {
-            completion(.failure(FetchError.invalidURL))
-            return
+    func fetchDailyBoxOffice(at date: String) async throws -> [DailyBoxOfficeList] {
+        let api = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)"
+        
+        guard let url = URL(string: api) else {
+            throw FetchError.invalidURL
         }
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(FetchError.invalidResponse))
-                return
-            }
-            
-            guard let data = data, let movie = try? JSONDecoder().decode(Movie.self, from: data) else {
-                completion(.failure(FetchError.invalidData))
-                return
-            }
-            
-            let dailyBoxOfficeList = movie.boxOfficeResult.dailyBoxOfficeList
-            
-            completion(.success(dailyBoxOfficeList))
-        }.resume()
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            throw FetchError.invalidResponse
+        }
+        
+        let movie = try JSONDecoder().decode(Movie.self, from: data)
+        
+        return movie.boxOfficeResult.dailyBoxOfficeList
     }
     
-    func fetchMovieDetail(code: String, completion: @escaping (Result<MovieInfo, Error>) -> Void) {
-        guard let url = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)") else {
-            completion(.failure(FetchError.invalidURL))
-            return
+    func fetchMovieDetail(code: String) async throws -> MovieInfo {
+        let api = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)"
+        
+        guard let url = URL(string: api) else {
+            throw FetchError.invalidURL
         }
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(FetchError.invalidResponse))
-                return
-            }
-            
-            guard let data = data, let movie = try? JSONDecoder().decode(MovieDetail.self, from: data) else {
-                completion(.failure(FetchError.invalidData))
-                return
-            }
-            
-            let movieInfo = movie.movieInfoResult.movieInfo
-            
-            completion(.success(movieInfo))
-        }.resume()
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            throw FetchError.invalidResponse
+        }
+        
+        let movieDetail = try JSONDecoder().decode(MovieDetail.self, from: data)
+        
+        return movieDetail.movieInfoResult.movieInfo
     }
 }
