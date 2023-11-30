edbd85c2921728ed966681667a6e4007f622e020
jyubong <nane030@naver.com>
2023-11-30 19:04:17 +0900

    refactor: 제네릭 타입 구현, test

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -9,16 +9,13 @@ import Foundation
 
 final class NetworkManager {
     private let urlSession: URLSessionProtocol
-    private let key = "3d65ed918572e0c8dc412bb3bf722f49"
     
     init(urlSession: URLSessionProtocol = URLSession.shared) {
         self.urlSession = urlSession
     }
-    
-    func fetchDailyBoxOffice(at date: String, completion: @escaping ([DailyBoxOfficeList]?, Error?) -> Void) {
-        let api = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)"
-        
-        guard let url = URL(string: api) else {
+
+    func fetchData<T: Decodable>(url: String, completion: @escaping (T?, Error?) -> Void) {
+        guard let url = URL(string: url) else {
             completion(nil, FetchError.invalidURL)
             return
         }
@@ -35,45 +32,12 @@ final class NetworkManager {
                 return
             }
             
-            guard let data = data, let movie = try? JSONDecoder().decode(Movie.self, from: data) else {
+            guard let data = data, let movie = try? JSONDecoder().decode(T.self, from: data) else {
                 completion(nil, FetchError.invalidData)
                 return
             }
             
-            let dailyBoxOfficeList = movie.boxOfficeResult.dailyBoxOfficeList
-            
-            completion(dailyBoxOfficeList, nil)
-        }.resume()
-    }
-    
-    func fetchMovieDetail(code: String, completion: @escaping (MovieInfo?, Error?) -> Void) {
-        let api = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)"
-        
-        guard let url = URL(string: api) else {
-            completion(nil, FetchError.invalidURL)
-            return
-        }
-        
-        urlSession.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode) else {
-                completion(nil, FetchError.invalidResponse)
-                return
-            }
-            
-            guard let data = data, let movie = try? JSONDecoder().decode(MovieDetail.self, from: data) else {
-                completion(nil, FetchError.invalidData)
-                return
-            }
-            
-            let movieInfo = movie.movieInfoResult.movieInfo
-            
-            completion(movieInfo, nil)
+            completion(movie, nil)
         }.resume()
     }
 }
--- NetworkManagerTests/NetworkManagerTests.swift
+++ NetworkManagerTests/NetworkManagerTests.swift
@@ -12,22 +12,34 @@ final class NetworkManagerTests: XCTestCase {
     var sut: NetworkManager!
 
     func test_fetchDailyBoxOffice() {
-        let url = URL(string: "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220105")!
-        let data = TestMovieJsonData.json.data(using: .utf8)!
+        // given
+        let api = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220105"
+        
+        guard let url = URL(string: api) else {
+            XCTFail()
+            return
+        }
+        
+        guard let data = TestMovieJsonData.json.data(using: .utf8) else {
+            XCTFail()
+            return
+        }
+        
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
         let dummy = DummyData(data: data, response: response, error: nil)
         let stubURLSession = StubURLSession(dummy: dummy)
         
         sut = .init(urlSession: stubURLSession)
         
-        var result: [DailyBoxOfficeList]?
+        let expectation: Movie? = try? JSONDecoder().decode(Movie.self, from: data)
+        var result: Movie?
         
-        sut.fetchDailyBoxOffice(at: "20220105") { dailyBoxOfficeList, error  in
-            result = dailyBoxOfficeList
+        // when
+        sut.fetchData<Movie>(url: api) { movie, error  in
+            result = movie
         }
         
-        let expectation: [DailyBoxOfficeList]? = try? JSONDecoder().decode(Movie.self, from: data).boxOfficeResult.dailyBoxOfficeList
-        
+        // then
         XCTAssertEqual(result, expectation)
         XCTAssertNotNil(result)
         XCTAssertNotNil(expectation)
