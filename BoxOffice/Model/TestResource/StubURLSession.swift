70911afc0e344104be3c8a2a9642c1219eb37df9
kiseok <carti1108@naver.com>
2023-12-01 12:02:57 +0900

    feat: stuburlsession, dummydata 타입 구현

--- BoxOffice/Model/TestResource/StubURLSession.swift
+++ BoxOffice/Model/TestResource/StubURLSession.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct DummyData {
+    let data: Data
+    let response: URLResponse
+}
+
+class StubURLSession: URLSessionProtocol {
+    var dummyData: DummyData
+
+    init(dummy: DummyData) {
+        self.dummyData = dummy
+    }
+    
+    func data(from url: URL, delegate: (URLSessionTaskDelegate)? = nil) async throws -> (Data, URLResponse) {
+        return (dummyData.data, dummyData.response)
+    }
+}
