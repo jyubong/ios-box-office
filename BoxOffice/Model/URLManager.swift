48dbc29e37877f82068d5bde7930909726c1cb98
kiseok <carti1108@naver.com>
2023-12-01 10:09:43 +0900

    feat: urlmanager 타입 구현

--- BoxOffice/Model/URLManager.swift
+++ BoxOffice/Model/URLManager.swift
@@ -2,7 +2,18 @@
 //  URLManager.swift
 //  BoxOffice
 //
-//  Created by Kiseok on 12/1/23.
+//  Created by jyubong, Kiseok on 12/1/23.
 //
 
-import Foundation
+struct URLManager {
+    private let baseURL = "http://kobis.or.kr/kobisopenapi/webservice/rest/"
+    private let key = "3d65ed918572e0c8dc412bb3bf722f49"
+    
+    func dailyBoxOfficeURL(date: String) -> String {
+        return "\(baseURL)boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)"
+    }
+    
+    func movieDetailURL(code: String) -> String {
+        return "\(baseURL)movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)"
+    }
+}
