a39644b095d60c7d9c8de825467aae9494b7bac6
jyubong <nane030@naver.com>
2023-12-01 11:52:52 +0900

    refactor: fetchData 제네릭 메서드로 수정

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -10,14 +10,10 @@ import Foundation
 final class NetworkManager {
     static let shared = NetworkManager()
     
-    private let key = "3d65ed918572e0c8dc412bb3bf722f49"
-    
     private init() { }
     
-    func fetchDailyBoxOffice(at date: String) async throws -> [DailyBoxOfficeList] {
-        let api = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)"
-        
-        guard let url = URL(string: api) else {
+    func fetchData<T: Decodable>(url: String, dataType: T.Type) async throws -> T {
+        guard let url = URL(string: url) else {
             throw FetchError.invalidURL
         }
         
@@ -28,27 +24,8 @@ final class NetworkManager {
             throw FetchError.invalidResponse
         }
         
-        let movie = try JSONDecoder().decode(Movie.self, from: data)
+        let movie = try JSONDecoder().decode(dataType, from: data)
         
-        return movie.boxOfficeResult.dailyBoxOfficeList
-    }
-    
-    func fetchMovieDetail(code: String) async throws -> MovieInfo {
-        let api = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)"
-        
-        guard let url = URL(string: api) else {
-            throw FetchError.invalidURL
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse,
-              (200...299).contains(httpResponse.statusCode) else {
-            throw FetchError.invalidResponse
-        }
-        
-        let movieDetail = try JSONDecoder().decode(MovieDetail.self, from: data)
-        
-        return movieDetail.movieInfoResult.movieInfo
+        return movie
     }
 }
