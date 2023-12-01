e25930479adaf6f5baed9b82d1ffd070938968cc
kiseok <carti1108@naver.com>
2023-12-01 10:10:03 +0900

    feat: fetch 메서드 제네릭을 이용하여 하나로 통합

--- BoxOffice/Model/NetworkManager.swift
+++ BoxOffice/Model/NetworkManager.swift
@@ -10,14 +10,10 @@ import Foundation
 final class NetworkManager {
     static let shared = NetworkManager()
     
-    private let key = "3d65ed918572e0c8dc412bb3bf722f49"
-    
     private init() { }
     
-    func fetchDailyBoxOffice(at date: String, completion: @escaping ([DailyBoxOfficeList]?, Error?) -> Void) {
-        let api = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(date)"
-        
-        guard let url = URL(string: api) else {
+    func fetchData<T: Decodable>(url: String, completion: @escaping (T?, Error?) -> Void) {
+        guard let url = URL(string: url) else {
             completion(nil, FetchError.invalidURL)
             return
         }
@@ -29,50 +25,17 @@ final class NetworkManager {
             }
             
             guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode) else {
+                  (200...299).contains(httpResponse.statusCode) else {
                 completion(nil, FetchError.invalidResponse)
                 return
             }
             
-            guard let data = data, let movie = try? JSONDecoder().decode(Movie.self, from: data) else {
+            guard let data = data, let movie = try? JSONDecoder().decode(T.self, from: data) else {
                 completion(nil, FetchError.invalidData)
                 return
             }
             
-            let dailyBoxOfficeList = movie.boxOfficeResult.dailyBoxOfficeList
-            
-            completion(dailyBoxOfficeList, nil)
-        }.resume()
-    }
-    
-    func fetchMovieDetail(code: String, completion: @escaping (MovieInfo?, Error?) -> Void) {
-        let api = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=\(key)&movieCd=\(code)"
-        
-        guard let url = URL(string: api) else {
-            completion(nil, FetchError.invalidURL)
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode) else {
-                completion(nil, FetchError.invalidResponse)
-                return
-            }
-            
-            guard let data = data, let movie = try? JSONDecoder().decode(MovieDetail.self, from: data) else {
-                completion(nil, FetchError.invalidData)
-                return
-            }
-            
-            let movieInfo = movie.movieInfoResult.movieInfo
-            
-            completion(movieInfo, nil)
+            completion(movie, nil)
         }.resume()
     }
 }
